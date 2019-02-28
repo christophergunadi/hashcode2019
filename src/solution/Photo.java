51cfb4993ffe2ee52d60a70e15f06ea6c8d9f7ab
jasminequah <jasminequah@outlook.com>
2019-02-28 18:02:35 +0000

    Create initial fields and methods for photo

--- src/solution/Photo.java
+++ src/solution/Photo.java
@@ -1,5 +1,21 @@
 package solution;
 
+import java.util.Set;
+
 public class Photo {
+
+  private int id;
+  private boolean isVertical;
+  private Set<String> tags;
+
+  public Photo(int id, boolean isVertical, Set<String> tags) {
+    this.id         = id;
+    this.isVertical = isVertical;
+    this.tags       = tags;
+  }
+
+  public Set<String> getTags() {
+    return tags;
+  }
 
 }
